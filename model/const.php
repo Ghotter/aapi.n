c5eca629b86952c68c224a7cdeb40cbd6044fb60
Ghotter <86737280+Ghotter@users.noreply.github.com>
2022-04-05 17:18:20 -0300

    Update const.php

--- model/const.php
+++ model/const.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','apigp.mysql.database.azure.com');
+define('DB_USER','victor@apigp');
+define('DB_PASS','azullcogu2@');
 define('DB_NAME','apigp');
